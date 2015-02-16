7a9641ddad4175bf2b7afe1be48a956ec33938ef
shakked <zshakked@stevens.edu>
2015-02-16 17:19:05 -0500

    First commit

---
+++ Get Short Usernames/Get Short Usernames/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Get Short Usernames
+//
+//  Created by Zachary Shakked on 2/16/15.
+//  Copyright (c) 2015 Shkeek Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
